de1ab3a9896638f53e72f70ffb9f4e005fb31e05
Abirami1594 <109016312+Abirami1594@users.noreply.github.com>
2023-03-25 18:40:59 +0530

    Add files via upload
    Codoid Website Task

---
+++ cod/cod.js
@@ -0,0 +1,12 @@
+const body = document.querySelector("body");
+
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY >0);
+    
+})
+const body1 = document.querySelector("con");
+
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY >0);
+    
+})
